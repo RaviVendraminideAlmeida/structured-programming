a55cf401af387a01331df463ae00eb69f4b96f77
Ravi <79207982+RaviVendraminideAlmeida@users.noreply.github.com>
2023-03-30 10:49:03 -0300

    Add files via upload

---
+++ aula2/ex02/ex02.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+int main(int argc, char** argv){
+	string name;
+
+	cout << "Type in your name: ";
+
+	cin >> name;
+
+	// "\n" / endl
+
+	cout << "\nHello, " << name << "\n";
+	
+	return 0;
+}
